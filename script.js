eb3a1c3563fa069c00b2846ff84740a80324bb00
yourusername <your@email.com>
2019-04-29 17:15:31 +0400

    avelacrel enq index.html script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(500,500);
+    background('#acacac');
+
+}
+
+function draw() {
+    var r = random (500);
+    var y = random (500);
+    ellipse(r,y,40,40);
+    fill(0,0,255);
+}
+
+/*Programming 3*/
